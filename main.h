d4eb248f064b48726823f333213777db078698cd
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-04-21 03:07:37 -0700

    added a prototype

--- main.h
+++ main.h
@@ -6,10 +6,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int _strlen(char *s);
+char *_strcat(char *dest, char *str);
+char *read_line(void);
 void exe_cmd(char **argv);
 char *getlocation(char *command);
-char *_strcat(char *dest, char *str);
 
 #endif
