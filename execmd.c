aeef22b573aa95701a355de765de29b195b8302f
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-04-21 03:02:09 -0700

    execute the command

---
+++ execmd.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * exe_cmd - execute the command
+ * @argv: array of tokens
+ */
+
+void exe_cmd(char **argv)
+{
+	char *command = NULL, *actual_command = NULL;
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (argv)
+		{
+			command = argv[0];
+			actual_command = getlocation(command);
+			if ((execve(actual_command, argv, NULL)) == -1)
+			{
+				perror("Error:");
+			}
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("error forking");
+	}
+	else
+	{
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+}
